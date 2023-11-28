949809e969102d62e4901888c1d87e97666accba
Ryan Capouellez <rjc8237@nyu.edu>
2023-11-27 22:21:21 -0500

    Set Conformal library to penner based branch

--- cmake/conformal_ideal_delaunay.cmake
+++ cmake/conformal_ideal_delaunay.cmake
@@ -3,7 +3,6 @@ FetchContent_Declare(
     conformal_ideal_delaunay
     SYSTEM
     GIT_REPOSITORY https://github.com/rjc8237/ConformalIdealDelaunay.git
-    GIT_TAG clean_layout
+    GIT_TAG penner_optimization
 )
 FetchContent_MakeAvailable(conformal_ideal_delaunay)
-
