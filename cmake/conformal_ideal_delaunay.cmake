2c0c10c7453bc857d833862ad0c4f138842ce45c
Ryan Capouellez <rjc8237@nyu.edu>
2024-05-17 13:21:27 -0400

    Fixed conformal repo tag

--- cmake/conformal_ideal_delaunay.cmake
+++ cmake/conformal_ideal_delaunay.cmake
@@ -3,6 +3,6 @@ FetchContent_Declare(
     conformal_ideal_delaunay
     SYSTEM
     GIT_REPOSITORY https://github.com/rjc8237/ConformalIdealDelaunay.git
-    GIT_TAG d139f38edfac4395951f38dd4a0c5b9cda1d79e3
+    GIT_TAG penner_optimization
 )
 FetchContent_MakeAvailable(conformal_ideal_delaunay)
