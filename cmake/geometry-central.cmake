1830b1d2e239512e2cf322fe851e5d21cb3372db
Ryan Capouellez <rjc8237@nyu.edu>
2024-10-04 15:41:58 -0400

    Fixed register and geometry central fetch

--- cmake/geometry-central.cmake
+++ cmake/geometry-central.cmake
@@ -7,5 +7,6 @@ FetchContent_Declare(
     geometry-central
     SYSTEM
     GIT_REPOSITORY https://github.com/nmwsharp/geometry-central.git
+    GIT_TAG 03f198748d263cea3faa040c8a399d59c38d67bc
 )
 FetchContent_MakeAvailable(geometry-central)
